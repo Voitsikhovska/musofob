94a471b6206c7e244d970c45c8e01c019b602c57
Voitsikhovska <111961494+Voitsikhovska@users.noreply.github.com>
2023-02-01 12:41:39 +0200

    sum

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -89,3 +89,9 @@ countries.forEach((country)=>{
     a.classList.add("listItem");
     list.appendChild(a);
 })*/
+const menuHamburger = document.querySelector(".menu-hamburger");
+const navLinks = document.querySelector(".nav-links");
+ 
+ menuHamburger.addEventListener('click',()=>{
+        navLinks.classList.toggle('mobile-menu')
+        })
